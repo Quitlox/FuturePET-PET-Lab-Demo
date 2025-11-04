a6c81e7a0cd29b9d9652673de7962b1862996d0a
Kevin Witlox <kevin.witlox@tno.nl>
2025-11-04 08:30:02 +0100

    feat: add secret sharing demo

--- src/tno/mpc/demo/futurepet/demo.py
+++ src/tno/mpc/demo/futurepet/demo.py
@@ -12,9 +12,12 @@ from tno.mpc.communication.communicators.http_communicator import (
 )
 from tno.mpc.encryption_schemes.paillier import Paillier
 from tno.mpc.encryption_schemes.paillier.paillier import PaillierCiphertext
+from tno.mpc.secret_sharing.shamir import (
+    ShamirSecretSharingScheme,
+)
 
 
-async def setup_pools() -> tuple[Pool, Pool]:
+async def setup_two_pools() -> tuple[Pool, Pool]:
     # Create Pool and Communicator for Alice
     comm_alice: Communicator = HttpCommunicator(addr="localhost", port=8100)
     pool_alice = Pool("alice", comm_alice)
@@ -35,9 +38,38 @@ async def setup_pools() -> tuple[Pool, Pool]:
     return pool_alice, pool_bob
 
 
+async def setup_three_pools() -> tuple[Pool, Pool, Pool]:
+    # Create Pool and Communicator for Alice
+    comm_alice: Communicator = HttpCommunicator(addr="localhost", port=8100)
+    pool_alice = Pool("alice", comm_alice)
+
+    # Create Pool and Communicator for Bob
+    comm_bob: Communicator = HttpCommunicator(addr="localhost", port=8101)
+    pool_bob = Pool("bob", comm_bob)
+
+    # Create Pool and Communicator for Charlie
+    comm_charlie: Communicator = HttpCommunicator(addr="localhost", port=8102)
+    pool_charlie = Pool("charlie", comm_charlie)
+
+    # Add connectionts
+    pool_alice.add_client(name="bob", connection=HttpConnection(addr="localhost", port=8101))
+    pool_alice.add_client(name="charlie", connection=HttpConnection(addr="localhost", port=8102))
+    pool_bob.add_client(name="alice", connection=HttpConnection(addr="localhost", port=8100))
+    pool_bob.add_client(name="charlie", connection=HttpConnection(addr="localhost", port=8102))
+    pool_charlie.add_client(name="alice", connection=HttpConnection(addr="localhost", port=8100))
+    pool_charlie.add_client(name="bob", connection=HttpConnection(addr="localhost", port=8101))
+
+    # Initialize
+    await pool_alice.initialize()
+    await pool_bob.initialize()
+    await pool_charlie.initialize()
+
+    return pool_alice, pool_bob, pool_charlie
+
+
 async def demo_communication() -> None:
     # Setup
-    pool_alice, pool_bob = await setup_pools()
+    pool_alice, pool_bob = await setup_two_pools()
 
     await pool_alice.send("bob", "hello world!")
     message: str = await pool_bob.recv("alice")
@@ -50,7 +82,7 @@ async def demo_communication() -> None:
 
 async def demo_homomorphic_encryption() -> None:
     # Setup communication
-    pool_alice, pool_bob = await setup_pools()
+    pool_alice, pool_bob = await setup_two_pools()
 
     # Demo
     async def alice() -> None:
@@ -84,6 +116,47 @@ async def demo_homomorphic_encryption() -> None:
     await asyncio.gather(*[pool_alice.shutdown(), pool_bob.shutdown()])
 
 
+async def demo_secret_sharing() -> None:
+    # Setup communication
+    pool_alice, pool_bob, pool_charlie = await setup_three_pools()
+
+    # Demo
+    async def alice() -> None:
+        shamir = ShamirSecretSharingScheme(3, 65535, 1, pool_alice)
+        x_shared = shamir.share("x", 1)
+        await x_shared.send()
+        y_shared = await shamir.receive("bob", "y")
+        z_shared = await shamir.receive("charlie", "z")
+        sum_shared = x_shared + y_shared + z_shared
+        await sum_shared.exchange()
+        print(sum_shared.reconstruct())
+
+    async def bob() -> None:
+        shamir = ShamirSecretSharingScheme(3, 65535, 1, pool_bob)
+        y_shared = shamir.share("y", 2)
+        await y_shared.send()
+        x_shared = await shamir.receive("alice", "x")
+        z_shared = await shamir.receive("charlie", "z")
+        sum_shared = x_shared + y_shared + z_shared
+        await sum_shared.exchange()
+        print(sum_shared.reconstruct())
+
+    async def charlie() -> None:
+        shamir = ShamirSecretSharingScheme(3, 65535, 1, pool_charlie)
+        z_shared = shamir.share("z", 3)
+        await z_shared.send()
+        x_shared = await shamir.receive("alice", "x")
+        y_shared = await shamir.receive("bob", "y")
+        sum_shared = x_shared + y_shared + z_shared
+        await sum_shared.exchange()
+        print(sum_shared.reconstruct())
+
+    # Shutdown
+    await asyncio.gather(*[alice(), bob(), charlie()])
+    await asyncio.gather(*[pool_alice.shutdown(), pool_bob.shutdown(), pool_charlie.shutdown()])
+
+
 if __name__ == "__main__":
     # asyncio.run(demo_communication())
-    asyncio.run(demo_homomorphic_encryption())
+    # asyncio.run(demo_homomorphic_encryption())
+    asyncio.run(demo_secret_sharing())
